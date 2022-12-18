dcd07cbdc802d674d284a0c5529626dd1376079a
Alexander Stensrud <alex@yuki.no>
2022-12-18 09:58:32 +0100

    Experimenting with konnectivity

---
+++ roles/konnectivity-agent/templates/konnectivity-agent.mk
@@ -0,0 +1,3 @@
+journalctl += -u konnectivity-agent.service
+systemctl += konnectivity-agent.service
+paths += /etc/konnectivity-agent /var/lib/konnectivity-agent /etc/systemd/system/konnectivity-agent.service
